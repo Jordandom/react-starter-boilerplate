f08c46aec9f63e562567284fab864ccd5b8cd84e
Jordan Domovčijski <jordan.domovcijski@valatron.com>
2023-10-22 17:18:05 +0200

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Button from '@components/Button';
+
+function App() {
+  return (
+    <>
+      <Button />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
